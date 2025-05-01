c971ddda47396c3ac9a0d176f71ecfe36c3e9675
Vipin Yadav <vipinvyadav9@gmail.com>
2025-05-01 21:03:15 +0530

    Signup Endpoint wip

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Streamify
+
+Date : 01/05/2025 Continue From 46:20 https://youtu.be/ZuwigEmwsTM?si=gl4ZmcXdJmswtujM&t=2780
---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,56 @@
+import User from "../models/user.js";
+import iwt from "jsonwebtoken";
+
+export async function signup(req, res) {
+  const { email, password, fullName } = req.body;
+
+  try {
+    if (!email || !password || !fullName) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Email already exists, please use a different one" });
+    }
+
+    const idx = Math.floor(Math.random() * 100) + 1;
+
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+    const newUser = new User.create({
+      email,
+      fullName,
+      password,
+      profilePix: randomAvatar,
+    });
+
+    const token = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "7d",
+      }
+    );
+  } catch (error) {}
+}
+
+export async function login(req, res) {
+  res.send("Login Route");
+}
+export function logout(req, res) {
+  res.send("Logout Route");
+}
